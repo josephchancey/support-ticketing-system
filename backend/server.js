c08919905b2dd11fcd8c243d9647b7d751110c91
josephchancey <josephchancey11@gmail.com>
2022-03-27 21:34:02 -0700

    Establish mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
 const { response } = require('express');
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 8000;
 
+// Database connection
+connectDB()
+
+//Init express
 const app = express();
 
+// Use Express to return JSON rather than HTML 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
 
 app.get('/', (req, res) => {
     res.status(200).send({message: 'Welcome to the support ticket API'})
 });
 
+// User Routing
 app.use('/api/users', require('./routes/userRoutes'));
 app.use(errorHandler);
 
