0a0a773721cc09fc6d20eddbc24a308edf8c80c2
josephchancey <josephchancey11@gmail.com>
2022-03-27 22:27:27 -0700

    Being user login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,7 +55,23 @@ const registerUser = asyncHandler( async (req, res) =>{
 // @route   /api/users/login
 // @access  Public
 const loginUser = asyncHandler( async (req, res) =>{
-    res.send('Login User Route')
+
+    const {email, password} = req.body;
+
+    // get user by email since email is unique in our schema 
+    const user = await User.findOne({email})
+
+    // Check if users password matches ecryption - if so then this is correct login
+    if(user && (await bcyrpt.compare(password, user.password))){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid login credentials')
+    }
 });
 
 
