a7b9dcffc9ee5b3302148601fbeb17e91485acfa
Joseph Chancey <josephchancey11@gmail.com>
2022-03-27 17:12:54 -0700

    Add user routes and controller

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
+const { response } = require('express');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send({message: 'Welcome to the support ticket API'})
+});
+
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
