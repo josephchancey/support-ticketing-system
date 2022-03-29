63521bac932fb94331f45bdb8f122f1694936266
josephchancey <josephchancey11@gmail.com>
2022-03-28 20:02:54 -0700

    Add input functionality for user registration

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,84 @@
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+
 function Register() {
-    return(
-        <div>
-            <h1>Register</h1>
-        </div>
-    )
+
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConf: "",
+  });
+
+  const { name, email, password, passwordConf } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name={name}
+              value={name}
+              onChange={onChange}
+              placeholder="Name"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name={email}
+              value={email}
+              onChange={onChange}
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name={password}
+              value={password}
+              onChange={onChange}
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="passwordConf"
+              name={passwordConf}
+              value={passwordConf}
+              onChange={onChange}
+              placeholder="Confirm Password"
+            />
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Register
+export default Register;
